4530441f61171f07fd1e2fb15d9c55c8108f0ebf
unknown <niranjan1xworkz@gmail.com>
2022-09-16 12:28:28 +0530

    program

---
+++ MobileNo.java
@@ -0,0 +1,40 @@
+class MobileNo
+{
+	static void getNameByMobile()
+{
+   System.out.println("entered getNameByMobile");
+   long mobile=7411851165L;
+   if(mobile==123456789L)
+   {
+	System.out.println("name is carrie and mobile no is 123456789");
+   }  
+   if(mobile==7894561230L)
+   {
+	System.out.println("name is john and mobile no is 7894561230");
+   }
+   if(mobile==9876543210L)
+   {
+	System.out.println("name is ame and mobile no is 9876543210 ");
+   }
+   if(mobile==7896541230L)
+   {
+	System.out.println("name is michale and mobile no is 7896541230");
+   }
+   if(mobile==9874563210L)
+   {
+	System.out.println("name is meera and mobile no is 9874563210");
+   }
+   if(mobile==123456789L)
+   {
+	System.out.println("name is nikhil and mobile no is 0123456789");
+   }
+   if(mobile==1478523690L)
+   {
+	System.out.println("name is ayushi and mobile no is 1478523690");
+   }
+   if(mobile==3698521470L)
+   {
+	System.out.println("name is jung and mobile no is 3698521470");
+   }
+}
+}
---
+++ MobileNoRunner.java
@@ -0,0 +1,5 @@
+class MobileNoRunner{
+	public static void main(String[] args){
+		MobileNo.getNameByMobile();
+	}
+}
